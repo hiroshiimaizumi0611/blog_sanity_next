e82b90821a57aa4a9d4ebee9302618d1f7830a1e
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-04-24 18:36:50 +0900

    page

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,8 +4,8 @@ import { ModeToggle } from "./ModeToggle";
 export default function NavBar() {
   return (
     <nav className="w-full flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
-      <Link href="/" className="font-bold text-3xl">
-        Marshall<span className="text-primary">Blog</span>
+      <Link href="/" className="font-bold text-3xl uppercase">
+        engineer<span className="text-primary">Blog</span>
       </Link>
       <ModeToggle />
     </nav>
